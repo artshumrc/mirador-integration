bde84b2619eccbdc2d601a76c03a2fc4b1facc41
Cole Crawford <cole_crawford@harvard.edu>
2020-11-23 15:29:07 -0500

    Giza customizations
    - Include package-lock.json and dist, with main.js for distribution
    - Update Mirador from RC5 to v3.0.0
    - Update to use new root level actions
    - Remove annotations
    - Update plugins; dl-plugin and share at latest do not work, using latest versions which don't throw errors
    - Customize theme, workspace, panels, window
    - Move config to build

--- src/index.js
+++ src/index.js
@@ -2,33 +2,40 @@ import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 import MiradorDownload from 'mirador-dl-plugin';
 import MiradorDownloadDialogPlugin from 'mirador-dl-plugin';
-import miradorAnnotationPlugins from 'mirador-annotations/es/index';
-import LocalStorageAdapter from 'mirador-annotations/es/LocalStorageAdapter';
 import miradorSharePlugin from 'mirador-share-plugin';
 import miradorShareDialog from 'mirador-share-plugin';
 
 const config = {
-  annotation: {
-    adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
-  },
   id: 'demo',
   window: {
-    defaultSideBarPanel: 'annotations',
+    allowClose: false,
+    allowFullscreen: true,
+    allowMaximize: false,
+    panels: {
+      annotations: false
+    }
+  },
+  workspace: {
+    draggingEnabled: false,
+    allowNewWindows: false,
+    showZoomControls: true
   },
   theme: {
     palette: {
       primary: {
-        main: '#1967d2',
+        main: '#731422',
       },
     },
   },
+  workspaceControlPanel: {
+    enabled: false
+  }
 };
 
 var miradorInstance = Mirador.viewer(config, [
   ...miradorImageToolsPlugin,
   MiradorDownload.miradorDownloadPlugin,
   MiradorDownload.MiradorDownloadDialogPlugin,
-  ...miradorAnnotationPlugins,
   miradorSharePlugin.miradorSharePlugin,
   miradorShareDialog.miradorShareDialog
 ]);
